b12b318977976b5f52a8881a030cb5ee00f3f71d
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2020-07-20 12:38:51 +0300

    support OnlineSchemaChange on schema/apply
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- go/vt/vtctld/api.go
+++ go/vt/vtctld/api.go
@@ -571,6 +571,7 @@ func initAPI(ctx context.Context, ts *topo.Server, actions *ActionRepository, re
 		req := struct {
 			Keyspace, SQL         string
 			ReplicaTimeoutSeconds int
+			OnlineSchemaChange    bool
 		}{}
 		if err := unmarshalRequest(r, &req); err != nil {
 			return fmt.Errorf("can't unmarshal request: %v", err)
@@ -585,8 +586,11 @@ func initAPI(ctx context.Context, ts *topo.Server, actions *ActionRepository, re
 		wr := wrangler.New(logger, ts, tmClient)
 
 		executor := schemamanager.NewTabletExecutor(
-			wr, time.Duration(req.ReplicaTimeoutSeconds)*time.Second)
-
+			wr, time.Duration(req.ReplicaTimeoutSeconds)*time.Second,
+		)
+		if req.OnlineSchemaChange {
+			executor.SetOnlineSchemaChange()
+		}
 		return schemamanager.Run(ctx,
 			schemamanager.NewUIController(req.SQL, req.Keyspace, w), executor)
 	})
